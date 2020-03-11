7a4f18a1a4c220bd4cb06a5622b612543a068004
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-03-11 20:36:51 -0300

    [FEAT] Aproveitando e otimizando mais o código

--- src/ExercicioDois.java
+++ src/ExercicioDois.java
@@ -3,16 +3,23 @@ public class ExercicioDois {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(possoIrAoBanco("Sábado", 15));
+		System.out.println(possoIrAoBanco("Sexta", 15));
 	}
 	
 	public static boolean possoIrAoBanco(String dia, int hora)
 	{
-		boolean dia_ativo = (dia != "Sábado" && dia !="Domingo");
-		boolean hora_ativa = (hora > 8 && hora < 19);
-		boolean posso_ir = (dia_ativo && hora_ativa);
+		final boolean dia_ativo = (dia != "Sábado" && dia !="Domingo");
+		final boolean hora_ativa = (hora > 8 && hora < 19);
+		final boolean posso_ir = (dia_ativo && hora_ativa && ehDiaSemana(dia));
+		
 		
 		return posso_ir;
+	}
+	
+	public static boolean ehDiaSemana(String dia)
+	{
+		boolean edia = (dia == "Segunda" || dia == "Terça" || dia == "Quarta" || dia == "Quinta" || dia == "Sexta" || dia == "Sábado" || dia == "Domingo");
+		return edia;
 	}
 
 }
