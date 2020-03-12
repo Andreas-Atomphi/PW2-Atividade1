4df895d6fd52ab59e9d710fe7de871e697d4a130
Andreas-Atomphi <andreasfariaen3@gmail.com>
2020-03-11 22:08:49 -0300

    [FIX] correção do horário

--- src/mainPack/ExercicioDois.java
+++ src/mainPack/ExercicioDois.java
@@ -12,7 +12,7 @@ public class ExercicioDois {
 	public static boolean possoIrAoBanco(String dia, int hora)
 	{
 		final boolean dia_ativo = !(dia.equalsIgnoreCase("Sábado") && dia.equalsIgnoreCase("Domingo"));
-		final boolean hora_ativa = (hora > 8 && hora < 19);
+		final boolean hora_ativa = (hora > 9 && hora < 17);
 		final boolean posso_ir = (dia_ativo && hora_ativa && ExercicioUm.ehDiaSemana(dia));
 		
 		
